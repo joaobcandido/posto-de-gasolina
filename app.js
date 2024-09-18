ad0d42a70c2324aa23f174d6708652676ec79eb5
joao candido <joan.jb56@gmail.com>
2024-09-18 12:12:46 -0300

    fixed text input bug

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 document.getElementById("formulario").addEventListener("submit", function (e) {
     e.preventDefault();
 
-    let tipoCombustivel = document.getElementById("tipo__conbustivel").value;
+    let tipoCombustivel = document.getElementById("tipo__conbustivel").value.toLowerCase();
     let litros = document.getElementById("litros").value;
     let precoGasolina = document.getElementById("preco__gasolina").value;
     let precoAlcool = document.getElementById("preco__alcool").value;
@@ -15,9 +15,7 @@ document.getElementById("formulario").addEventListener("submit", function (e) {
     
     if (tipoCombustivel === "gasolina"){
         resultado.textContent = `Você abasteceu com ${litros} litros de gasolina, o valor a pagar é : ${totalGasolina}`;
-        resultado.style.color = "green"
     } else{
         resultado.textContent = `Você abasteceu com ${litros} litros de alcool, o valor a pagar é : ${totalAlcool}`;
-        resultado.style.color = "green"
     }
 });
