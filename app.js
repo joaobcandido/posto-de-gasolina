5669536884d5df9b9530ab73c0b3661869814ca4
joao candido <joan.jb56@gmail.com>
2024-09-17 21:16:24 -0300

    update

--- app.js
+++ app.js
@@ -1,14 +1,23 @@
 document.getElementById("formulario").addEventListener("submit", function (e) {
     e.preventDefault();
 
-
+    let tipoCombustivel = document.getElementById("tipo__conbustivel").value;
     let litros = document.getElementById("litros").value;
-    let precoGasolina = document.getElementById("preco").value;
-    let totalAPagar = (litros * precoGasolina).toFixed(2);
+    let precoGasolina = document.getElementById("preco__gasolina").value;
+    let precoAlcool = document.getElementById("preco__alcool").value;
+
+    let totalGasolina = (litros * precoGasolina).toFixed(2);
+    let totalAlcool = (litros * precoAlcool).toFixed(2);
 
 
     let resultado = document.getElementById("resultado");
 
     
-    resultado.textContent = `Você abasteceu com ${litros} litros o valor a pagar é :${totalAPagar}  `;
+    if (tipoCombustivel === "gasolina"){
+        resultado.textContent = `Você abasteceu com ${litros} litros de gasolina, o valor a pagar é : ${totalGasolina}`;
+        resultado.style.color = "green"
+    } else{
+        resultado.textContent = `Você abasteceu com ${litros} litros de alcool, o valor a pagar é : ${totalAlcool}`;
+        resultado.style.color = "green"
+    }
 });
