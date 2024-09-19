7829430194e943cfc8b0b8bc5e97d72e9a4cd9bf
joao candido <joan.jb56@gmail.com>
2024-09-19 20:07:42 -0300

    added diesel option

--- app.js
+++ app.js
@@ -5,9 +5,11 @@ document.getElementById("formulario").addEventListener("submit", function (e) {
     let litros = document.getElementById("litros").value;
     let precoGasolina = document.getElementById("preco__gasolina").value;
     let precoAlcool = document.getElementById("preco__alcool").value;
+    let precoDiesel = document.getElementById("preco__diesel").value;
 
     let totalGasolina = (litros * precoGasolina).toFixed(2);
     let totalAlcool = (litros * precoAlcool).toFixed(2);
+    let totalDiesel = (litros * precoDiesel).toFixed(2);
 
 
     let resultado = document.getElementById("resultado");
@@ -17,6 +19,8 @@ document.getElementById("formulario").addEventListener("submit", function (e) {
         resultado.textContent = `Você abasteceu com ${litros} litros de gasolina, o valor a pagar é : ${totalGasolina}`;
     } else if (tipoCombustivel === "alcool"){
         resultado.textContent = `Você abasteceu com ${litros} litros de alcool, o valor a pagar é : ${totalAlcool}`;
+    } else if (tipoCombustivel === "diesel"){
+        resultado.textContent = `Você abasteceu com ${litros} litros de diesel, o valor a pagar é : ${totalDiesel}`;   
     } else {
         resultado.textContent = "voçê escolheu um combustivel que não esta disponivel"
     }
